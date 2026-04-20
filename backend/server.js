6b8234308e47ece5f431933880a471076af8133a
Louqmane Bamousse <louqmanebamousse@gmail.com>
2026-04-20 17:14:44 +0000

    deploy bacend on github codespace

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,7 @@ app.use("/api/trips", tripRouter);
 app.use(errorHandler)
 
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () =>
-//   console.log(`Server running on http://localhost:${PORT}`)
-// );
-
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
